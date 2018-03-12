cfb2cbc70186da557aafda144ed0d31d2c4c8587
ashleysmithgpu <ashleysmithgpu@gmail.com>
2018-03-12 11:30:22 +0000

    Updated for Ubuntu 18.04

--- fish/config.fish
+++ fish/config.fish
@@ -7,15 +7,11 @@ set -x HISTFILESIZE
 set -x HISTSIZE
 
 # try to fix all the fucked up ssh stuff
-set -x SSH_AUTH_SOCK /run/user/1001/keyring/ssh
+#set -x SSH_AUTH_SOCK /run/user/1001/keyring/ssh
 
-set -x DISPLAY ":0"
+#set -x DISPLAY ":0"
 
-if test -d "$HOME/.local/bin"
-    set -x PATH "$HOME/.local/bin" $PATH
-end
-
-. $HOME/.local/lib/python2.7/site-packages/powerline/bindings/fish/powerline-setup.fish
+. /usr/share/powerline/bindings/fish/powerline-setup.fish
 
 powerline-setup
 
